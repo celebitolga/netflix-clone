22f0f0d1df4ea91783490505b7274101bb038bc5
Tolga-Celebi <tolgacelebi78@gmail.com>
2020-12-25 18:43:39 +0300

    Footer finished

--- src/app/home/footer/footer.component.ts
+++ src/app/home/footer/footer.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  langSwitch: string = "Türkçe";
+
   constructor() { }
 
   ngOnInit(): void {
